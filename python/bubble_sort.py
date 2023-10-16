f9280d94d0e315ca17be0193fb8b7e70c46d6393
annascriv <scriven.anna@outlook.com>
2023-10-16 17:22:44 -0500

    bubble sort

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,7 +1,20 @@
 sequence = [4, 3, 5, 0, 1]
 swaps = 0
 
-# Your Code Here
+swapped = bool
+
+while swapped:
+    swapped = False
+    for i in range(len(sequence)-1):
+        if sequence[i]>sequence[i+1]:
+            temp = sequence[i]
+            sequence[i]=sequence[i+1]
+            sequence[i+1]=temp
+            swapped=True
+            swaps+=1
+result = sequence
+
+
 
 print(f"Final result: {result}")
 print(f"Swaps: {swaps}")
